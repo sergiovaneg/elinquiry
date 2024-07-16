e5546fd14b200ab9dceb7264ecb0ad8a990e7a74
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2024-07-16 10:10:28 +0300

    Refactored 'Some Assembly Required' and added typespec.

--- lib/advent_of_code/y2015/assembly_required/assembly_required.exs
+++ lib/advent_of_code/y2015/assembly_required/assembly_required.exs
@@ -1,85 +1,81 @@
 alias AdventOfCode.Y2015.AssemblyRequired
 
 defmodule AdventOfCode.Y2015.AssemblyRequired do
+  @typep cache :: %{optional(binary()) => non_neg_integer()}
+  @typep entry :: (registry(), cache() -> cache())
+  @typep registry :: %{binary() => entry()}
+
+  @spec safe_query(binary(), registry(), cache()) ::
+          {non_neg_integer(), cache()}
   defp safe_query(operand, registry, cache) do
-    if Regex.match?(~r/[0-9]+/, operand) do
-      {operand |> String.to_integer(), cache}
+    if Regex.run(~r/[0-9]+/, operand, capture: :first) == [operand] do
+      {operand |> String.to_integer() |> Bitwise.band(0xFFFF), cache}
     else
       cache = registry[operand].(registry, cache)
       {cache[operand], cache}
     end
   end
 
+  @spec parse_instruction(binary()) :: {binary(), entry()}
   defp parse_instruction(line) do
-    operands = line |> String.split(~r/( -> | )/)
+    operands = line |> String.split(~r/ -> | /)
     target = operands |> List.last()
 
     case operands |> length() do
       2 ->
-        %{
-          target => fn registry, cache ->
+        {
+          target,
+          fn registry, cache ->
             if cache |> Map.has_key?(target) do
               cache
             else
-              {num, cache} =
-                operands
-                |> List.first()
-                |> safe_query(registry, cache)
+              {x, cache} = operands |> Enum.at(0) |> safe_query(registry, cache)
 
-              cache |> Map.put(target, num)
+              cache |> Map.put(target, x)
             end
           end
         }
 
-      # Only 'NOT' fits this pattern
+      # Only 'NOT' fits this case
       3 ->
-        %{
-          target => fn registry, cache ->
+        {
+          target,
+          fn registry, cache ->
             if cache |> Map.has_key?(target) do
               cache
             else
               {x, cache} = operands |> Enum.at(1) |> safe_query(registry, cache)
-
-              cache
-              |> Map.put(
-                target,
-                x
-                |> Bitwise.bnot()
-                |> Bitwise.band(0xFFFF)
-              )
+              cache |> Map.put(target, x |> Bitwise.bxor(0xFFFF))
             end
           end
         }
 
       4 ->
-        %{
-          target => fn registry, cache ->
+        op =
+          case operands |> Enum.at(1) do
+            "AND" ->
+              &Bitwise.band/2
+
+            "OR" ->
+              &Bitwise.bor/2
+
+            "LSHIFT" ->
+              &Bitwise.bsl/2
+
+            "RSHIFT" ->
+              &Bitwise.bsr/2
+          end
+
+        {
+          target,
+          fn registry, cache ->
             if cache |> Map.has_key?(target) do
               cache
             else
               {x, cache} = operands |> Enum.at(0) |> safe_query(registry, cache)
               {y, cache} = operands |> Enum.at(2) |> safe_query(registry, cache)
 
-              res =
-                case operands |> Enum.at(1) do
-                  "AND" ->
-                    x |> Bitwise.band(y)
-
-                  "OR" ->
-                    x |> Bitwise.bor(y)
-
-                  "LSHIFT" ->
-                    x
-                    |> Bitwise.bsl(y)
-                    |> Bitwise.band(0xFFFF)
-
-                  "RSHIFT" ->
-                    x
-                    |> Bitwise.bsr(y)
-                    |> Bitwise.band(0xFFFF)
-                end
-
-              cache |> Map.put(target, res)
+              cache |> Map.put(target, x |> op.(y) |> Bitwise.band(0xFFFF))
             end
           end
         }
@@ -92,22 +88,14 @@ defmodule AdventOfCode.Y2015.AssemblyRequired do
       contents
       |> String.split("\n")
       |> Enum.map(&parse_instruction/1)
-      |> Enum.reduce(%{}, &Map.merge/2)
+      |> Map.new()
 
     registry["a"].(registry, %{})["a"]
   end
 
+  @spec part2(binary(), non_neg_integer()) :: non_neg_integer()
   def part2(contents, a) do
-    contents
-    |> String.split("\n")
-    |> Enum.map(fn x ->
-      if(x |> String.ends_with?("-> b")) do
-        "#{a} -> b"
-      else
-        x
-      end
-    end)
-    |> Enum.join("\n")
+    (contents <> "\n#{a} -> b")
     |> part1()
   end
 end
