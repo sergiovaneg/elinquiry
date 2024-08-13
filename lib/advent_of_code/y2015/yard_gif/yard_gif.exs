b192d3bb79c2f2cb2a9dc561e091830d4cd44ba7
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2024-08-13 10:53:21 +0300

    Solved day 18, 2015 (Like a GIF for your Yard) using padding and chunk_every.

---
+++ lib/advent_of_code/y2015/yard_gif/yard_gif.exs
@@ -0,0 +1,98 @@
+alias AdventOfCode.Y2015.YardGif
+
+defmodule AdventOfCode.Y2015.YardGif do
+  @type grid :: [[boolean(), ...], ...]
+
+  @spec run_step(grid()) :: grid()
+  defp run_step(grid) do
+    padding = for _ <- 1..length(grid), do: false
+
+    ([padding] ++ grid ++ [padding])
+    |> Enum.chunk_every(3, 1, :discard)
+    |> Enum.map(fn triple ->
+      padding = [false, false, false]
+
+      ([padding] ++
+         (triple
+          |> Enum.zip_with(fn x -> x end)) ++ [padding])
+      |> Enum.chunk_every(3, 1, :discard)
+      |> Enum.map(fn neighbourhood ->
+        [_, [_, m, _], _] = neighbourhood
+
+        count =
+          neighbourhood
+          |> List.flatten()
+          |> List.replace_at(4, false)
+          |> Enum.count(fn x -> x end)
+
+        if m do
+          count == 2 or count == 3
+        else
+          count == 3
+        end
+      end)
+    end)
+  end
+
+  @spec set_corners(grid()) :: grid()
+  defp set_corners(grid) do
+    grid
+    |> List.update_at(0, fn row ->
+      row |> List.replace_at(0, true) |> List.replace_at(-1, true)
+    end)
+    |> List.update_at(-1, fn row ->
+      row |> List.replace_at(0, true) |> List.replace_at(-1, true)
+    end)
+  end
+
+  @spec part1(binary()) :: non_neg_integer()
+  def part1(contents) do
+    1..100
+    |> Enum.reduce(
+      contents
+      |> String.split("\n")
+      |> Enum.map(fn line ->
+        line
+        |> String.graphemes()
+        |> Enum.map(fn c -> c == "#" end)
+      end),
+      fn _, g ->
+        g |> run_step()
+      end
+    )
+    |> List.flatten()
+    |> Enum.count(fn x -> x end)
+  end
+
+  @spec part2(binary()) :: non_neg_integer()
+  def part2(contents) do
+    1..100
+    |> Enum.reduce(
+      contents
+      |> String.split("\n")
+      |> Enum.map(fn line ->
+        line
+        |> String.graphemes()
+        |> Enum.map(fn c -> c == "#" end)
+      end)
+      |> set_corners(),
+      fn _, g ->
+        g
+        |> run_step()
+        |> set_corners()
+      end
+    )
+    |> List.flatten()
+    |> Enum.count(fn x -> x end)
+  end
+end
+
+contents = File.read!("./input.txt") |> String.trim()
+
+contents
+|> YardGif.part1()
+|> IO.puts()
+
+contents
+|> YardGif.part2()
+|> IO.puts()
