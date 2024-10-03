c3270c46e65377a6402c0542df7715da1b384301
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2024-10-03 11:14:16 +0200

    Removed aux functions.

--- lib/advent_of_code/y2016/like_rogue/like_rogue.exs
+++ lib/advent_of_code/y2016/like_rogue/like_rogue.exs
@@ -13,31 +13,21 @@ defmodule AdventOfCode.Y2016.LikeRogue do
     |> Enum.map(fn [l, _, r] -> l == r end)
   end
 
-  defp count_safe(contents, n_rows) do
+  def count_safe(contents, n_rows) do
     1..n_rows
     |> Enum.reduce({contents |> parse_row(), 0}, fn _, {last_row, acc} ->
       {last_row |> get_next(), acc + (last_row |> Enum.count(& &1))}
     end)
     |> then(fn {_, n_safe} -> n_safe end)
   end
-
-  def part1(contents) do
-    contents
-    |> count_safe(40)
-  end
-
-  def part2(contents) do
-    contents
-    |> count_safe(400_000)
-  end
 end
 
 contents = File.read!("./input.txt") |> String.trim()
 
 contents
-|> LikeRogue.part1()
+|> LikeRogue.count_safe(40)
 |> IO.puts()
 
 contents
-|> LikeRogue.part2()
+|> LikeRogue.count_safe(400_000)
 |> IO.puts()
