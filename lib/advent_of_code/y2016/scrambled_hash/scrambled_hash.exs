cf8a7ac373c3c2d3fe4d8e3c0d16f6db0f7fc4e0
Sergio Mauricio Vanegas Arias <sergiovaneg@gmail.com>
2024-11-12 14:09:25 +0100

    Code duplication BEGONE.

--- lib/advent_of_code/y2016/scrambled_hash/scrambled_hash.exs
+++ lib/advent_of_code/y2016/scrambled_hash/scrambled_hash.exs
@@ -4,12 +4,8 @@ defmodule AdventOfCode.Y2016.ScrambledHash do
   @clean_passwd "abcdefgh"
   @scrambled_passwd "fbgdceah"
 
-  @spec execute(binary(), binary()) :: binary()
-  defp execute(inst, passwd) do
-    split_passwd =
-      passwd
-      |> String.graphemes()
-
+  @spec execute(binary(), [binary()], :fwd | :bwd) :: [binary()]
+  defp execute(inst, passwd, mode \\ :fwd) do
     case inst |> String.split(" ") do
       ["swap", criterion, x, _w, _, y] ->
         [i1, i2] =
@@ -19,140 +15,107 @@ defmodule AdventOfCode.Y2016.ScrambledHash do
           else
             [x, y]
             |> Enum.map(fn target ->
-              split_passwd
+              passwd
               |> Enum.find_index(&(target == &1))
             end)
           end
 
-        e1 = split_passwd |> Enum.at(i1)
-        e2 = split_passwd |> Enum.at(i2)
+        [e1, e2] = [i1, i2] |> Enum.map(&Enum.at(passwd, &1))
 
-        split_passwd
+        passwd
         |> List.replace_at(i1, e2)
         |> List.replace_at(i2, e1)
 
       ["rotate", dir, n, _] ->
-        mult = if(dir == "left", do: 1, else: -1)
+        mult =
+          case {dir, mode} do
+            conf when conf in [{"right", :bwd}, {"left", :fwd}] ->
+              1
 
-        split_passwd
-        |> Enum.split(mult * rem(String.to_integer(n), length(split_passwd)))
+            conf when conf in [{"right", :fwd}, {"left", :bwd}] ->
+              -1
+          end
+
+        passwd
+        |> Enum.split(mult * rem(String.to_integer(n), length(passwd)))
         |> then(fn {l, r} -> [r, l] |> List.flatten() end)
 
       ["rotate" | tail] ->
-        idx =
-          split_passwd
-          |> Enum.find_index(&(tail |> List.last() == &1))
-          |> then(fn idx ->
-            if idx >= 4 do
-              idx + 2
-            else
-              idx + 1
-            end
-          end)
-          |> rem(length(split_passwd))
+        case mode do
+          :fwd ->
+            shift =
+              passwd
+              |> Enum.find_index(&(tail |> List.last() == &1))
+              |> then(fn shift ->
+                if shift >= 4 do
+                  shift + 2
+                else
+                  shift + 1
+                end
+              end)
+              |> rem(length(passwd))
 
-        split_passwd
-        |> Enum.split(-idx)
-        |> then(fn {l, r} -> [r, l] |> List.flatten() end)
+            passwd
+            |> Enum.split(-shift)
+            |> then(fn {l, r} -> [r, l] |> List.flatten() end)
 
-      ["reverse", _pos, x, _to, y] ->
-        [i1, i2] = [x, y] |> Enum.map(&String.to_integer/1)
-
-        {b, t} = split_passwd |> Enum.split(i2 + 1)
-        {b, m} = b |> Enum.split(i1)
-
-        [b | [m |> Enum.reverse() | t]]
-
-      ["move", _pos, x, _to, _, y] ->
-        [i1, i2] = [x, y] |> Enum.map(&String.to_integer/1)
-
-        {b, [m | t]} = split_passwd |> Enum.split(i1)
-
-        (b ++ t)
-        |> List.insert_at(i2, m)
-    end
-    |> Enum.join()
-  end
-
-  @spec reverse(binary(), binary()) :: binary()
-  defp reverse(inst, passwd) do
-    split_passwd =
-      passwd
-      |> String.graphemes()
-
-    case inst |> String.split(" ") do
-      ["swap", criterion, x, _w, _, y] ->
-        [i1, i2] =
-          if criterion == "position" do
-            [x, y]
-            |> Enum.map(&String.to_integer/1)
-          else
-            [x, y]
-            |> Enum.map(fn target ->
-              split_passwd
-              |> Enum.find_index(&(target == &1))
+          :bwd ->
+            0..length(passwd)
+            |> Enum.reduce_while(passwd, fn _, src ->
+              if(execute(inst, src) == passwd) do
+                {:halt, src}
+              else
+                [h | t] = src
+                {:cont, t ++ [h]}
+              end
             end)
-          end
-
-        e1 = split_passwd |> Enum.at(i1)
-        e2 = split_passwd |> Enum.at(i2)
-
-        split_passwd
-        |> List.replace_at(i1, e2)
-        |> List.replace_at(i2, e1)
-
-      ["rotate", dir, n, _] ->
-        mult = if(dir == "left", do: -1, else: 1)
-
-        split_passwd
-        |> Enum.split(mult * rem(String.to_integer(n), length(split_passwd)))
-        |> then(fn {l, r} -> [r, l] |> List.flatten() end)
-
-      ["rotate" | _tail] ->
-        0..length(split_passwd)
-        |> Enum.reduce_while(split_passwd, fn _, src ->
-          if(execute(inst, src |> Enum.join()) == passwd) do
-            {:halt, src}
-          else
-            [h | t] = src
-
-            {
-              :cont,
-              t ++ [h]
-            }
-          end
-        end)
+        end
 
       ["reverse", _pos, x, _to, y] ->
         [i1, i2] = [x, y] |> Enum.map(&String.to_integer/1)
 
-        {b, t} = split_passwd |> Enum.split(i2 + 1)
-        {b, m} = b |> Enum.split(i1)
+        {h, t} = passwd |> Enum.split(i2 + 1)
+        {h, m} = h |> Enum.split(i1)
 
-        [b | [m |> Enum.reverse() | t]]
+        h ++ (m |> Enum.reverse()) ++ t
 
       ["move", _pos, x, _to, _, y] ->
-        [i1, i2] = [x, y] |> Enum.map(&String.to_integer/1)
+        [i1, i2] =
+          if mode == :fwd do
+            [x, y]
+          else
+            [y, x]
+          end
+          |> Enum.map(&String.to_integer/1)
 
-        {b, [m | t]} = split_passwd |> Enum.split(i2)
-
-        (b ++ t)
-        |> List.insert_at(i1, m)
+        passwd
+        |> Enum.split(i1)
+        |> then(fn {h, [m | t]} ->
+          (h ++ t)
+          |> List.insert_at(i2, m)
+        end)
     end
-    |> Enum.join()
   end
 
   def part1(contents) do
     contents
     |> String.split("\n")
-    |> Enum.reduce(@clean_passwd, &execute/2)
+    |> Enum.reduce(
+      @clean_passwd |> String.graphemes(),
+      &execute(&1, &2, :fwd)
+    )
+    |> Enum.join()
   end
 
   def part2(contents) do
     contents
     |> String.split("\n")
     |> Enum.reverse()
-    |> Enum.reduce(@scrambled_passwd, &reverse/2)
+    |> Enum.reduce(
+      @scrambled_passwd |> String.graphemes(),
+      &execute(&1, &2, :bwd)
+    )
+    |> Enum.join()
   end
 end
 
